3ac8bf8d953e8e26f0f31aa48aeb03ab261eb2d8
Oksana Koyfman <okildavdan@comcast.net>
2018-09-12 21:11:36 -0400

    Cleaned database file

--- js/offlinedb.js
+++ js/offlinedb.js
@@ -1,10 +1,4 @@
 var dbPromise = idb.open('restaurants', 1, function(upgradeDb) {
-    switch (upgradeDb.oldVersion) {
-      case 0:
         var keyValStore =  upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
-        keyValStore.put("world", "hello");
-      case 1:
-      var keyValStore = upgradeDb.createObjectStore('restaurantsReviews', {keyPath: 'id'});
-      keyValStore.put("world", "hello");
     }
 });
